0151b29612998dc3562476e4ad959b26f902f040
Vitor Veira <manoelvitor506@gmail.com>
2023-06-10 19:19:44 -0300

    segundo commit

--- Desafio Primo/numerosPrimos.js
+++ Desafio Primo/numerosPrimos.js
@@ -1,17 +1,18 @@
 // Entre com um número para saber se ele é primo e retorne uma mensagem validando. 
 // Crie uma função e use console.log.
+
 function numPrimo() {
     
-    var numero = 0
+    let numero = 0
     
     while (numero < 100) {
             
     numero = numero + 1;
 
-    if ((numero == 1) || ((numero != 2) && (numero % 2 == 0)) || ((numero != 3) && (numero % 3 == 0))) {
+    if ((numero == 1) || ((numero != 2) && (numero % 2 == 0)) || ((numero != 3) && (numero % 3 == 0)) || ((numero != 5) && (numero % 5 == 0)) || (numero != 7) && (numero % 7 == 0)) {
         console.log(numero +' '+ "Não é primo.");
     }else {
-        if ((numero > 1) || (numero == 5) || (numero == 7)) {
+        if ((numero > 1) || (numero == 5)) {
             console.log(numero +' '+ "Primo.");
         }
     }
